f3ff640fd219df7e18c1a9db3efa41f22b95314d
SquarePear <16364318+SquarePear@users.noreply.github.com>
2021-02-24 19:34:59 -0500

    :sparkles: Add command shorthand

--- src/cli.ts
+++ src/cli.ts
@@ -8,14 +8,19 @@ import { run as runServe } from "./commands/serve.ts";
 const args = parse(Deno.args);
 
 export async function run() {
+  if (args._.length == 0) return;
+
   switch (args._[0].toString().toLowerCase()) {
     case "create":
+    case "c":
       await runCreate(args);
       return;
     case "new":
+    case "n":
       await runNew(args);
       return;
     case "serve":
+    case "s":
       await runServe(args);
       return;
     default:
